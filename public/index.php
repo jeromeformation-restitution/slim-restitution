1f96d269962f466b0d0342a6880b258c018dd89d
jerome <ambroise.jerome.formation@gmail.com>
2019-03-26 09:59:29 +0100

    group de route et creation controller + namespace

--- public/index.php
+++ public/index.php
@@ -1,2 +1,31 @@
 <?php
-echo 'Hello la Classe!';
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->group('/project', function () {
+    $this->get('/{id:\d+}', function (Request $request, Response $response, array $args) {
+        $name = $args['id'];
+        $response->getBody()->write("Hello, $name");
+
+        return $response;
+    });
+    $this->get('/creation', function (Request $request, Response $response, array $args) {
+        $response->getBody()->write("Hello depuis création");
+
+        return $response;
+    });
+});
+
+
+$app->run();
